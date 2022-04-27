a47a3e635209665df386c25802111ae5d7f325db
tsmart123 <smarttolly@gmail.com>
2022-04-27 16:40:51 +0200

    Initial commit

---
+++ Assets/Audio/ImpactSound.cs
@@ -0,0 +1,18 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour
+{
+    public AudioSource impactSound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.relativeVelocity.magnitude > 0.1)
+        {
+            impactSound.Play();
+        }
+    }
+
+}
---
+++ Assets/ElevatorFloor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorFloor : MonoBehaviour
+{
+    Animator _doorAnim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _doorAnim.SetBool("isOpening", true);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnim = this.transform.parent.GetComponent<Animator>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
